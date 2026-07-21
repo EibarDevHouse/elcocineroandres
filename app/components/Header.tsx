f1613cd2ec8d89d39a8e136ebfc60e2242197adc
EibarDevHouse <ehernandez@ladevhouse.com>
2026-07-21 16:59:18 -0400

    added logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Layout.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img width={200} src="/logo.png" />
+      <img width={200} src="/logo-green.png" />
       {/* <div className={styles.headerTitle}>El Cocinero Andrés</div> */}
     </header>
   );
