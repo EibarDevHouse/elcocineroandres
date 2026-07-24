137953c8fc7d70f31cd4a73439fb934b3e9bbc3d
EibarDevHouse <ehernandez@ladevhouse.com>
2026-07-24 12:48:21 -0400

    redirect homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-import { getReviews } from './lib/reviews';
-import Header from './components/Header';
-import HomePage from './components/HomePage';
-import Navigation from './components/Navigation';
-import styles from './components/ElCocineroAndres.module.css';
+import { redirect } from 'next/navigation';
 
-export const metadata = {
-  title: 'Inicio - El Cocinero Andrés',
-  description: 'Sabor que se apodera de tus sentidos',
-};
-
-export default async function Home() {
-  const reviews = await getReviews();
-
-  return (
-    <div className={styles.container}>
-      <Header />
-      <HomePage reviews={reviews} />
-      <Navigation />
-    </div>
-  );
+export default function Home() {
+  redirect('/menu');
 }
